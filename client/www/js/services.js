85bc0d16cff6a6199dfda98dba109c2e338e124f
Brandon Fulk <brandon.fulk@gmail.com>
2015-03-19 23:49:58 -0500

    adding the first files

---
+++ client/www/js/services.js
@@ -0,0 +1,15 @@
+angular.module('starter.services', ['ngResource'])
+
+    .factory('Episode', function ($resource){
+	return $resource('http://archive.uhhyeahdu.de/episodes/:episodeSlug.json', {}, {
+		get: {
+		    method: 'GET',
+		    transformResponse: function (data, headersGetter) {
+			return JSON.parse(data);
+		    }
+		},
+		query: {
+		    method: 'GET'
+		}
+	    });
+    });
